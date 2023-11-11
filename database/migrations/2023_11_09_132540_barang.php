e74b9e3dde166bf19b4b7f9cca7d52a6b924fbe6
Luqman Hakim Naufal <luqmanhakimnaufal@yahoo.co.id>
2023-11-11 23:26:53 +0700

    first-commit

---
+++ database/migrations/2023_11_09_132540_barang.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbl_barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->integer('id_jenis')->nullable();
+            $table->biginteger('harga')->nullable();
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbl_barang');
+    }
+};
